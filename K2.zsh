54b968748652b4e5d5f762679078d9ee4551b85b
Lucas Gouvêa (K2) <lucas.k2@jupiter>
2017-10-21 15:28:18 -0200

    Dotfiles | VIMRC SUPERSAYAJIN 1.0 + removendo duplicatas surgidas da burrice

--- K2.zsh
+++ K2.zsh
@@ -17,6 +17,7 @@ alias cnpj='xsel -b < ~/.cnpj.txt'
 
 alias soma_on='cd /etc/openvpn/client && sudo openvpn --config Firewall-udp-1194.ovpn'
 source ~/devops/devops
+source ~/update_dotfiles.sh
 alias dit='docker exec -it'
 alias dps='docker ps'
 alias dcu='docker-compose up'
@@ -24,3 +25,8 @@ alias dcd='docker-compose down'
 alias dab='docker exec -it arteris_php bash'
 alias vimrc='vim ~/.vimrc'
 alias ssn='sudo shutdown now'
+alias laraup='docker-compose up -d workspace postgres pgadmin php-fpm nginx'
+alias ticsol='cd /var/www/tic-sms/laradock'
+alias ticup='cd /var/www/tic-sms/laradock && docker-compose up -d workspace postgres pgadmin php-fpm nginx'
+alias cmatrix='cmatrix -bas'
+alias v='vim'
