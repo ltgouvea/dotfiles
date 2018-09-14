724bf9cb6e15532f5ab87c9092f74a052f71c567
Lucas Gouvêa (K2) <lucas.fim@gmail.com>
2018-09-13 21:49:55 -0300

    master | bonito

--- scripts/reload_dunst.sh
+++ scripts/reload_dunst.sh
@@ -8,13 +8,14 @@
  reload_dunst() {
     pkill dunst
     dunst \
-        -lb "${color1}" \
-        -nb "${color1}" \
-        -cb "${color1}" \
-        -lf "${color4}" \
-        -bf "${color4}" \
-        -cf "${color4}" \
-        -nf "${color4}" &
+	-frame_width 0 \
+        -lb "${color0}" \
+        -nb "${color0}" \
+        -cb "${color0}" \
+        -lf "${color7}" \
+        -bf "${color7}" \
+        -cf "${color7}" \
+        -nf "${color7}" &
 }
 
 reload_dunst
