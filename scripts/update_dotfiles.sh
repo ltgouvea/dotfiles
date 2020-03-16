edcfed6342eb1e9d1032db16d08fb16e75cf0058
Lucas Gouvêa (K2) <lucas.fim@gmail.com>
2020-03-15 22:30:33 -0300

    path adjustments on scripts

--- scripts/update_dotfiles.sh
+++ scripts/update_dotfiles.sh
@@ -2,10 +2,10 @@
 
 dotfiles_push()
 {
-    cd ~/dotfiles/
+    cd ~/.dotfiles/
     cp ~/.K2.fish K2.fish
     cp ~/.git.fish git.fish
-    cp -r ~/config/i3* .
+    cp -r ~/.config/i3* .
     cp -r ~/.ncmpcpp/ .
     cp -r ~/.config/rofi .
     cp -r ~/.config/fish .
@@ -18,13 +18,13 @@ dotfiles_push()
 dotfiles_pull()
 {
     cd ~
-    cp -r ~/dotfiles/config/i3* ./.config/
-    cp -r ~/dotfiles/.ncmpcpp/ .
-    cp -r ~/dotfiles/.config/rofi ./.config/
-    cp -r ~/dotfiles/.config/fish/ ./.config/
-    cp ~/dotfiles/.vimrc .
-    cp ~/dotfiles/.vim/* ~/.vim/
-    cp ~/dotfiles/.Xdefaults .
-    cp ~/dotfiles/K2.fish ~/.K2.fish
-    cp ~/dotfiles/git.fish ~/.git.fish
+    cp -r ~/.dotfiles/.config/i3* ./.config/
+    cp -r ~/.dotfiles/.ncmpcpp/ .
+    cp -r ~/.dotfiles/.config/rofi ./.config/
+    cp -r ~/.dotfiles/.config/fish/ ./.config/
+    cp ~/.dotfiles/.vimrc .
+    cp ~/.dotfiles/.vim/* ~/.vim/
+    cp ~/.dotfiles/.Xdefaults .
+    cp ~/.dotfiles/K2.fish ~/.K2.fish
+    cp ~/.dotfiles/git.fish ~/.git.fish
 }
