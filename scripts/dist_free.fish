e079f48ced5c200752c455e77fb4cbcb4dd39230
Lucas Gouvêa <lucas.fim@gmail.com>
2020-03-29 00:58:32 -0300

    master: upgrade distraction free script and add a fish shell version

--- scripts/dist_free.fish
+++ scripts/dist_free.fish
@@ -1,12 +1,14 @@
-#!/bin/sh
-dist_free_disable() {
+#!/usr/bin/fish
+
+function dist_free_disable
         export DIST_FREE_MODE=0
         sudo sed -i '/facebook/d;/twitter/d;/reddit/d;/whatsapp/d' /etc/hosts
         echo 'dist_free: distraction free disabled.'
-}
+end
 
-dist_free_enable() {
+function dist_free_enable
         export DIST_FREE_MODE=1
+        echo 'enabling.'
         sudo sed -i '$a\
 0.0.0.0 www.facebook.com \
 0.0.0.0 facebook.com \
@@ -16,11 +18,11 @@ dist_free_enable() {
 0.0.0.0 reddit.com \
 0.0.0.0 www.web.whatsapp.com \
 0.0.0.0 web.whatsapp.com' /etc/hosts
-        echo 'dist_free: distraction free enabled.'
-}
+        echo 'dist_free: distraction free enabled'
+end
 
-dist_free_toggle() {
-        [ $DIST_FREE_MODE -eq 0 ] && dist_free_enable  || dist_free_disable
-}
+function dist_free_toggle
+        test "$DIST_FREE_MODE" -eq 0 && dist_free_enable  || dist_free_disable
+end
 
 
